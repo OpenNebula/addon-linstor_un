cc62992002a5a99a9f905286a1ef31ee02a90074
kvaps <kvapss@gmail.com>
2019-04-14 14:26:47 +0200

    code refactoring
    Signed-off-by: kvaps <kvapss@gmail.com>

--- datastore/linstor_un/linstor_utils.sh
+++ datastore/linstor_un/linstor_utils.sh
@@ -40,7 +40,82 @@ linstor_vd_size() {
         select(.vlm_nr==0).vlm_size"
 }
 
+#--------------------------------------------------------------------------------
+# Read environment variables and generate keys for linstor commands
+#   Gets environment variables:
+#   - LS_CONTROLLERS
+#   - REPLICAS_ON_SAME
+#   - REPLICAS_ON_DIFFERENT
+#   - AUTO_PLACE
+#   - DO_NOT_PLACE_WITH
+#   - DO_NOT_PLACE_WITH_REGEX
+#   - LAYER_LIST
+#   - ENCRYPTION
+#   Sets environment variables:
+#   - LINSTOR
+#   - VOL_CREATE_ARGS
+#   - RES_CREATE_ARGS
+#--------------------------------------------------------------------------------
+linstor_load_keys() {
+    if [ -n "$LS_CONTROLLERS" ]; then
+        LINSTOR="$LINSTOR --controllers $LS_CONTROLLERS"
+    fi
+    if [ -n "$REPLICAS_ON_SAME" ]; then
+        RES_CREATE_ARGS="$RES_CREATE_ARGS --replicas-on-same $REPLICAS_ON_SAME"
+    fi
+    if [ -n "$REPLICAS_ON_DIFFERENT" ]; then
+        RES_CREATE_ARGS="$RES_CREATE_ARGS --replicas-on-different $REPLICAS_ON_DIFFERENT"
+    fi
+    if [ -n "$AUTO_PLACE" ]; then
+        RES_CREATE_ARGS="$RES_CREATE_ARGS --auto-place $AUTO_PLACE"
+    fi
+    if [ -n "$DO_NOT_PLACE_WITH" ]; then
+        RES_CREATE_ARGS="$RES_CREATE_ARGS --do-not-place-with $DO_NOT_PLACE_WITH"
+    fi
+    if [ -n "$DO_NOT_PLACE_WITH_REGEX" ]; then
+        RES_CREATE_ARGS="$RES_CREATE_ARGS --do-not-place-with-regex $DO_NOT_PLACE_WITH_REGEX"
+    fi
+    if [ -n "$LAYER_LIST" ]; then
+        RES_CREATE_ARGS="$RES_CREATE_ARGS --layer-list $LAYER_LIST"
+    fi
+    if [ "$ENCRYPTION" = "yes" ]; then
+        VOL_CREATE_ARGS="$VOL_CREATE_ARGS --encrypt"
+    fi
+}
 
+#--------------------------------------------------------------------------------
+# Deploy new volume on linstor
+#   @param $1 the volume (to create)
+#   @param $2 the size
+#   Gets environment variables:
+#   - VOL_CREATE_ARGS
+#   - RES_CREATE_ARGS
+#--------------------------------------------------------------------------------
+linstor_deploy_volume() {
+    local RES="$1"
+    local SIZE="$2"
+
+    if [ -z "$NODE_LIST" ] && [ -z "$AUTO_PLACE" ]; then
+        error_message "Datastore template missing 'NODE_LIST' or 'AUTO_PLACE' attribute."
+        exit -1
+    fi
+
+    local REGISTER_CMD=$(cat <<EOF
+        set -e -o pipefail
+    
+        $LINSTOR resource-definition create "$RES"
+        trap '$LINSTOR resource-definition delete "$RES" --async' EXIT
+        ( exec $LINSTOR volume-definition create $VOL_CREATE_ARGS "$RES" "$SIZE" )
+        ( exec $LINSTOR resource create $RES_CREATE_ARGS --storage-pool "$STORAGE_POOL" $NODE_LIST "$RES" )
+        trap '' EXIT
+EOF
+)
+
+    multiline_exec_and_log "$REGISTER_CMD" \
+                    "Error registering $DST"
+
+
+}
 
 #--------------------------------------------------------------------------------
 # Attach diskless resource to the node if not exist
@@ -48,11 +123,11 @@ linstor_vd_size() {
 #   @param $2 the volume (to attach)
 #   @param $3 the diskless storage pool name (optional)
 #--------------------------------------------------------------------------------
-linstor_attach_resource() {
-    NODE="$1"
-    RES="$2"
-    DISKLESS_POOL="${3:-DfltDisklessStorPool}"
-    ATTACH_CMD=$(cat <<EOF
+linstor_attach_volume() {
+    local NODE="$1"
+    local RES="$2"
+    local DISKLESS_POOL="${3:-DfltDisklessStorPool}"
+    local ATTACH_CMD=$(cat <<EOF
         set -e -o pipefail
     
         CREATED_RES=\$($LINSTOR -m resource list -r "$RES" -n "$NODE" | $JQ -r '.[].resources[].name')
@@ -69,16 +144,21 @@ EOF
 # Detach diskless resource from the node if exist and diskless
 #   @param $1 the node
 #   @param $2 the volume (to detach)
+#   @param $3 async execution (1 - yes, 0 - no)
 #--------------------------------------------------------------------------------
-linstor_detach_resource() {
-    NODE="$1"
-    RES="$2"
-    DETACH_CMD=$(cat <<EOF
+linstor_detach_volume() {
+    local NODE="$1"
+    local RES="$2"
+    local ASYNC="${3:-0}"
+    if [ "$ASYNC" -eq 1 ]; then
+        local ASYNC_ARG="--async"
+    fi
+    local DETACH_CMD=$(cat <<EOF
         set -e -o pipefail
     
         DISKLESS_RES=\$($LINSTOR -m resource list -r "$RES" -n "$NODE" | $JQ -r '.[].resources[] | select(.rsc_flags[] | contains("DISKLESS")) | .name')
         if [ -n "\$DISKLESS_RES" ]; then
-            $LINSTOR resource delete "$NODE" "$RES" 
+            $LINSTOR resource delete "$NODE" "$RES" $ASYNC_ARG
         fi
 EOF
 )
@@ -96,27 +176,27 @@ EOF
 #   @return host to be used as bridge
 #-------------------------------------------------------------------------------
 function get_destination_host_for_res {
-    RES="$1"
-    MUST_CONTAIN="${2:-0}"
-    RR_ID="$3"
+    local RES="$1"
+    local MUST_CONTAIN="${2:-0}"
+    local RR_ID="$3"
 
-    IMAGE_NODES="$($LINSTOR -m resource list -r "${RES}" | $JQ -r '.[].resources[].node_name' | xargs)"
+    local IMAGE_NODES="$($LINSTOR -m resource list -r "${RES}" | $JQ -r '.[].resources[].node_name' | xargs)"
     unset REDUCED_LIST
     for NODE in $BRIDGE_LIST; do
         if [[ " $IMAGE_NODES " =~ " $NODE " ]] ; then
-            REDUCED_LIST="$REDUCED_LIST $NODE"
+            local REDUCED_LIST="$REDUCED_LIST $NODE"
         fi
     done
 
-    REDUCED_LIST=$(remove_off_hosts "$REDUCED_LIST")
+    local REDUCED_LIST=$(remove_off_hosts "$REDUCED_LIST")
     if [ -n "$REDUCED_LIST" ]; then
-        HOSTS_ARRAY=($REDUCED_LIST)
-        N_HOSTS=${#HOSTS_ARRAY[@]}
+        local HOSTS_ARRAY=($REDUCED_LIST)
+        local N_HOSTS=${#HOSTS_ARRAY[@]}
 
         if [ -n "$RR_ID" ]; then
-            ARRAY_INDEX=$(($RR_ID % ${N_HOSTS}))
+            local ARRAY_INDEX=$(($RR_ID % ${N_HOSTS}))
         else
-            ARRAY_INDEX=$((RANDOM % ${N_HOSTS}))
+            local ARRAY_INDEX=$((RANDOM % ${N_HOSTS}))
         fi
 
         echo ${HOSTS_ARRAY[$ARRAY_INDEX]}
